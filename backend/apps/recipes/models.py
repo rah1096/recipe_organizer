9fc8cc0f5df530e240a24cfe739f35d81b2d8306
Tim <chent1101@gmail.com>
2015-04-08 17:13:51 -0600

    Start up files for recipe project.

---
+++ backend/apps/recipes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    directions = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
